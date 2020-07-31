6470244bc63794762b5ec4c83a260067ba30b2eb
chapel tandel <dev_damani@yahoo.com>
2020-07-31 14:28:50 +0100

    Second commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace Generate_API_Key
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JObject videogameRatings = new JObject(
+            new JProperty("ApiKey", "ThisIsASampleApiKey"));
+
+            File.WriteAllText(@"videogames.json", videogameRatings.ToString());
+
+            // write JSON directly to a file
+            using (StreamWriter file = File.CreateText(@"videogames.json"))
+            using (JsonTextWriter writer = new JsonTextWriter(file))
+            {
+                videogameRatings.WriteTo(writer);
+            }
+        }
+    }
+}
